2279d10aa26f4bb21c2327cdb66ed2de7f87ed2c
boatybits <bitsboaty@gmail.com>
2019-04-22 19:56:43 +0100

    update

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,283 @@
 #include <Arduino.h>
+#include "myheader.h"
 
-void setup() {
-  // put your setup code here, to run once:
+//*****************************************************************************************************************************************
+//                         Run Setup
+//*****************************************************************************************************************************************
+
+void setup()
+{
+  Serial.begin(115200);
+  Wire.begin(5, 4); // pins 5 & 4 for wemos
+  LogOn();
+
+  BE280.setI2CAddress(0x76); //Connect to BME on ox76
+  if (BE280.beginI2C() == false)
+    Serial.println("BE280 connect failed");
+  else
+    Serial.println("BE280 connected");
+  delay(250); // let the BME settle down
+
+  ina219.begin();
+  ina219.setCalibration_16V_400mA();
+
+  ads.setGain(GAIN_TWOTHIRDS);
+  ads.begin();
+}
+//________________________________________________________________________________
+
+//*****************************************************************************************************************************************
+//                         Run Main Loop
+//*****************************************************************************************************************************************
+void loop()
+{
+
+  client.loop();
+  currentMillis = millis();
+  send_Data(1000);
+}
+//________________________________________________________________________________
+
+//*****************************************************************************************************************************************
+//*****************************************************************************************************************************************
+//                         SUBROUTINES
+//*****************************************************************************************************************************************
+//*****************************************************************************************************************************************
+
+void LogOn()
+{
+  Serial.println("Booting");
+  WiFi.disconnect(true); // disconnects STA Mode
+  delay(1000);
+  WiFi.softAPdisconnect(true); // disconnects AP Mode
+  delay(1000);
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  WiFi.setSleep(false);
+  while (WiFi.waitForConnectResult() != WL_CONNECTED)
+  {
+    Serial.println("Connection Failed! Rebooting...");
+    delay(5000);
+    ESP.restart();
+  }
+  // Log on successful
+  Serial.println("Logged on");
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+  Serial.println("Now setting up MQTT");
+
+  //________________MQTT stuff____________________________________________
+  client.setServer(mqttServer, mqttPort);
+  client.setCallback(callback);
+  while (!client.connected())
+  {
+    Serial.println("Connecting to MQTT...");
+    if (client.connect("ESP8266Client", mqttUser, mqttPassword))
+    {
+      Serial.println("connected");
+      delay(50);
+      sendMQTT("esp/text", "Booting");
+    }
+    else
+    {
+      Serial.print("failed with state ");
+      Serial.println(client.state());
+      delay(100);
+    }
+  }
+
+  client.publish("esp/text", "WEMOS started");
+  int subcheck = client.subscribe("esp/control");
+  Serial.print("subscribe = ");
+  Serial.println(subcheck);
+  client.subscribe("espcontrol");
+  client.subscribe("esp/controlcurrent");
+}
+//________________________________________________________________________________
+
+//                     callback ( ) - runs when MQTT message comes in
+//*******************************************************
+void callback(char *intopic, byte *payload, unsigned int length)
+{
+
+  Serial.print("Message arrived in topic: ");
+  Serial.println(intopic);
+
+  String readString = "";
+  for (int i = 0; i < length; i++)
+  {
+    delay(2); //delay to allow byte to arrive in input buffer
+    char c = payload[i];
+    readString += c;
+  }
+  // Serial.print("Message:");
+  // Serial.println(readString);
+
+  if (strcmp(intopic, "esp/control") == 0)
+  { //0 is true
+    switch (readString.toInt())
+    { // Node red sends intigers to topic esp/control from dash buttons
+    case 1:
+    {
+      sendMQTT("esp/text", "enabled = 1");
+    }
+    break;
+    case 2:
+    {
+      sendMQTT("esp/text", "enabled = 0");
+    }
+    break;
+    case 3:
+    {
+      sendMQTT("esp/text", "Serial print turned on"); //serial on
+      quickprint_flag = 1;
+    }
+    break;
+    case 4:
+    {
+      sendMQTT("esp/text", "Serial print turned off"); // serial off
+      quickprint_flag = 0;
+    }
+    break;
+    case 5:
+      sendMQTT("esp/text", "Reset Ah");
+
+      break;
+    case 6:
+
+      break;
+    case 7:
+      if (sendSig_Flag == 1)
+        sendSig_Flag = 0;
+      else if (sendSig_Flag == 0)
+        sendSig_Flag = 1;
+      sendMQTT("esp/text", String(sendSig_Flag));
+      break;
+    case 8:
+    {
+      sendMQTT("esp/text", "Restarting ESP");
+      delay(10);
+      ESP.restart();
+    }
+    break;
+    }
+  }
+  if (strcmp(intopic, "esp/controlcurrent") == 0)
+  { //0 is true
+    //    currentTarget = readString.toInt();
+    Serial.println(readString.toInt());
+    Serial.println(intopic);
+  }
+}
+//________________________________________________________________________________
+
+//                     sendMQTT ( )
+//*******************************************************
+void sendMQTT(String topic, String message)
+{
+  char pubString[message.length() + 1];
+  // make array message size plus /n
+  message.toCharArray(pubString, message.length() + 1);
+  char sendtopic[topic.length() + 1];
+  topic.toCharArray(sendtopic, topic.length() + 1);
+  //publish the data to MQTT
+  client.publish(sendtopic, pubString);
+}
+//___________________________________________________________
+
+// Quick print function
+//*******************************************************
+void qp(String to_print, int linefeed)
+{
+  if (linefeed == 0 && quickprint_flag == 1)
+    Serial.print(to_print);
+  else if (linefeed == 1 && quickprint_flag == 1)
+    Serial.println(to_print);
+}
+//________________________________________________________________________________
+
+//                     send signalk data over UDP
+//*******************************************************
+void sendSigK(String sigKey, float data)
+{
+
+  if (sendSig_Flag == 1)
+  {
+    DynamicJsonBuffer jsonBuffer;
+    String deltaText;
+
+    //  build delta message
+    JsonObject &delta = jsonBuffer.createObject();
+
+    //updated array
+    JsonArray &updatesArr = delta.createNestedArray("updates");
+    JsonObject &thisUpdate = updatesArr.createNestedObject();   //Json Object nested inside delta [...
+    JsonArray &values = thisUpdate.createNestedArray("values"); // Values array nested in delta[ values....
+    JsonObject &thisValue = values.createNestedObject();
+    thisValue["path"] = sigKey;
+    thisValue["value"] = data;
+
+    thisUpdate["Source"] = "ESP32";
+
+    // Send UDP packet
+    Udp.beginPacket(remoteIp, remotePort);
+    delta.printTo(Udp);
+    Udp.println();
+    Udp.endPacket();
+  }
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-}
+//___________________________________________________________
+
+void send_Data(int send_Data_Rate)
+
+{
+
+ 
+
+  //is it time to process the next state code?
+  if (currentMillis - timer1_Millis < send_Data_Rate)
+  {
+    //No, it is not time
+    return;
+  }
+
+  if (currentMillis - timer1_Millis > send_Data_Rate)
+  {
+    //Yes, it is now time
+
+    // Pressure ---------------------------------------------------------------
+    float BMEpressure = BE280.readFloatPressure();
+    float BMEtemp = BE280.readTempC();
+
+    sendSigK("environment.outside.pressure", BMEpressure);
+    sendSigK("environment.inside.temperature", (BMEtemp + 273.15));
+    qp("Pressure:", 0);
+    qp(String(BMEpressure), 1);
+    qp("Temp:", 0);
+    qp(String(BMEtemp), 1);
+
+    // Current ---------------------------------------------------------------
+    float current_mA = 0;
+    current_mA = ina219.getCurrent_mA();
+    qp("Current mA:", 0);
+    qp(String(current_mA), 1);
+    sendMQTT("esp/current", String(current_mA));
+    sendSigK("esp.meter.current", current_mA);
+
+    // Voltage ---------------------------------------------------------------
+    float multiplier = 0.1875F; /* ADS1115  @ +/- 6.144V gain (16-bit results) */
+    int16_t results;
+    results = ads2.readADC_Differential_0_1();
+    Serial.print("Differential: ");
+    Serial.print(results);
+    Serial.print("(");
+    Serial.print(results * multiplier);
+    Serial.println("mV)");
+
+    timer1_Millis = currentMillis; //reset timing
+  }
+
+} //                E n d  o f  s e n d D a t a ( )
+
+//___________________________________________________________
---
+++ src/myheader.h
@@ -0,0 +1,56 @@
+#pragma once // header guard so the file only runs once
+
+#include <Arduino.h>
+#include <WiFi.h>
+#include <WiFiClient.h>
+#include "PubSubClient.h" // MQTT
+#include "SparkFunBME280.h"
+#include "ArduinoJson.h"
+#include <Adafruit_INA219.h> //https://github.com/adafruit/Adafruit_INA219
+#include "Adafruit_ADS1015.h"
+
+// call functions so they can be put under main loop and compiler still sees them first
+void LogOn();
+void callback(char *intopic, byte *payload, unsigned int length);
+void sendMQTT(String topic, String message);
+void sendSigK(String sigKey, float data);
+void send_Data(int send_Data_Rate);
+
+// Wifi credentials
+const char *ssid = "openplotter";
+const char *password = "12345678";
+const IPAddress mqttServer(10, 10, 10, 1);
+const int mqttPort = 1883;
+const char *mqttUser = "cafonhvu";
+const char *mqttPassword = "Usp_VhNLl897";
+const PROGMEM char MQTT_CLIENT_ID[] = "esp32Client";
+
+const IPAddress remoteIp(10, 10, 10, 1); //
+const unsigned short remotePort = 55561; // Signalk listens on this port
+const unsigned short localPort = 6666;   // local port to listen for UDP packets
+
+// Timers for loops
+int send_Data_Rate = 1000; // loop time in mS for a2_sendData_Rate
+int check_online_Rate = 100;
+
+unsigned long timer1_Millis; // send data
+unsigned long timer2_Millis = 10000;
+unsigned long timer3_Millis = 1000;
+unsigned long timer4_Millis = 1000;
+unsigned long timer5_Millis = 1000;
+unsigned long timer6_Millis = 1000;
+
+// Flags
+int quickprint_flag = 1;
+int sendSig_Flag = 1;
+int sendInfdb_Flag = 1;
+unsigned long currentMillis;
+
+//                         Start instances of libraries
+WiFiClient espClient;
+PubSubClient client(espClient); // MQTT
+BME280 BE280;                   //BME280 barometer
+WiFiUDP Udp;                    // A UDP instance to let us send and receive packets over UDP
+Adafruit_INA219 ina219;
+Adafruit_ADS1115 ads (0x4A);
+Adafruit_ADS1115 ads2(0x4A);
