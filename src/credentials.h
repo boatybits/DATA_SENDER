7bca44f93ced8f211343a3da2dda280bd2378434
boatybits <bitsboaty@gmail.com>
2019-04-23 11:40:02 +0100

    update

--- src/credentials.h
+++ src/credentials.h
@@ -7,6 +7,6 @@ const char *ssid = "openplotter";
 const char *password = "12345678";
 const IPAddress mqttServer(10, 10, 10, 1);
 const int mqttPort = 1883;
-const char *mqttUser = "YOUR USER NAME";
-const char *mqttPassword = "YOUR PASSWORD";
+const char *mqttUser = "cafonhvu";
+const char *mqttPassword = "Usp_VhNLl897";
 const PROGMEM char MQTT_CLIENT_ID[] = "esp32Client";
